f9d4f14895b884e8bd6baf80d613ce44630ac700
Aurorable <33343160+Aurorable@users.noreply.github.com>
2017-11-03 06:55:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,23 +9,32 @@ client.on("ready", () => {
 
 //test function
 client.on("message", (message) => {
+  if (message.author.bot) return;
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }
 });
-
-client.login(process.env.BOT_TOKEN);
 
 //FUNCTIONS GO BELOW HERE
 //Bot Owner Only Commands
 client.on("message", (message)=>{
   let prefix=".";
   if(!message.content.startsWith(prefix)) return;
-  if(message.author != process.env.OWNER_ID){
+  if(message.author.id != OWNER_ID){
     message.channel.send("You do not have permission to access this command.");
+    return;
   }
-  params = message.content.split(" ").slice(1);
+  //const params = message.content.split(" ").slice(1);
   if(message.content.startsWith(prefix + "test")){
-    message.channel.send("Hello @"+OWNER_ID+"!");
+    message.channel.send("Hello <@261759589941248022>!");
+  }
+  if(message.content.startsWith(prefix + "setGame")){
+    const params = message.content.split(" ").slice(1);
+    //string builder
+    var tempGameName ="";
+    for(var gameNameInCounter=0; gameNameInCounter<params.length; gameNameInCounter++){
+      tempGameName=tempGameName+(params[gameNameInCounter]+" ");
+    }
+    client.user.setPresence({game: {name: tempGameName, type: 0}});
   }
 });
